df68773d5d433c20e836605b39d829c04296c877
Julie Jakobsen <juliehage@live.dk>
2022-08-26 10:20:59 +0200

    second commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      morkerod: "#400D0D",
+      lyseblaa: "#A3ACBF",
+      morkebrun: "#593719",
+      lysebrun: "#8C745E",
+      grå: "#D9D9D9",
+      hvid: "#FFFFFF",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
